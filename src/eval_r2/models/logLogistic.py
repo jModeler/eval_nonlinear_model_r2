33529027ed22d005821657e5d05a8e0b8b7a1a57
Swarnalatha Satyavolu <srs282@cornell.edu>
2025-06-14 08:46:33 -0400

    Completed the LogLogisticModel class

--- src/eval_r2/models/logLogistic.py
+++ src/eval_r2/models/logLogistic.py
@@ -1,5 +1,7 @@
 import numpy as np
+from scipy.special import expit
 from base import BaseKernel
+
 
 class LogLogisticModel(BaseKernel):
     def __init__(self, name: str):
@@ -8,5 +10,18 @@ class LogLogisticModel(BaseKernel):
     def model(self, b: float, d: float, e: float, x: np.array, model: str = "L3", f: float = 1.0, c: float = 1.0):
         # compute the appropriate kernel
         self.kernel(b, e, x, kernel_flag="loglinear") # this will create self.log_linear
-        # compute the fraction 1/(1 + exp(b*log(x)-e))
-        # To prevent numerical overflow, we'll 
+        # compute the fraction 1/(1 + exp(b*log(x)-e)), since this is common to all model types in this class
+        # we're using expit method from scipy to deal with extreme values in the self.log_linear array
+        self.sigmoid = expit(self.log_linear)
+        # now calculate the model predictions
+        if model == "L3":
+            self.predictions = d * self.sigmoid 
+        elif model == "L4":
+            self.predictions = c + (d-c) * self.sigmoid 
+        elif model == "L5":
+            self.predictions = c + (d-c) * np.float_power(self.sigmoid, f)
+        else:
+            raise ValueError(
+                "Invalid model. Please provide either 'L3', 'L4' or 'L5'."
+            )
+
