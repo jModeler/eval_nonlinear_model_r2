a0ff441b49302825da80c29f21e29230f906e85d
Swarnalatha Satyavolu <srs282@cornell.edu>
2025-06-14 08:40:52 -0400

    Fixed the extension class name to model from kernel

--- src/eval_r2/models/logLogistic.py
+++ src/eval_r2/models/logLogistic.py
@@ -1,15 +1,12 @@
 import numpy as np
 from base import BaseKernel
 
-class LogLogisticKernel(BaseKernel):
+class LogLogisticModel(BaseKernel):
     def __init__(self, name: str):
         super().__init__(name)
 
     def model(self, b: float, d: float, e: float, x: np.array, model: str = "L3", f: float = 1.0, c: float = 1.0):
         # compute the appropriate kernel
         self.kernel(b, e, x, kernel_flag="loglinear") # this will create self.log_linear
-        # compute the predicted values
-        if model == "L3":
-            # Three parameter log-logistic model
-
-
+        # compute the fraction 1/(1 + exp(b*log(x)-e))
+        # To prevent numerical overflow, we'll 
